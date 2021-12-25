821e03f33900924ca4357a0c5692a3ad13f4d23d
QLabs <55121845+Quantalabs@users.noreply.github.com>
2021-12-25 18:18:19 +0000

    Add express for 24/7 hosting and repl config file for hosting on replit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
